362aa176d38842094c70c25141b68dad6a5ee042
Pawel Szymanski <pawel.szymanski@emention.pl>
2015-02-12 19:37:37 -0800

    added gulp pipeline; some directories/paths renaming;

---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+
+var gulp =   require('gulp'),
+    coffee = require('gulp-coffee'),
+    sass =   require('gulp-sass'),
+    haml =   require('gulp-haml'),
+    concat = require('gulp-concat');
+
+
+
+var sources = {
+  docs:        {
+    images:      './src/docs/images/**/*.{jpg, jpeg, png, gif,}',
+    scripts:     './src/docs/scripts/**/*.coffee',
+    stylesheets: './src/docs/stylesheets/docs.sass',
+    webfonts:    './src/docs/webfonts/**/*.{eot, ttf, woff, svg}',
+    pages:       './src/docs/**/*.haml'
+  },
+  lib:         {
+    scripts:     './src/lib/scripts/**/*.coffee',
+    stylesheets: './src/lib/stylesheets/lib.sass',
+    templates:   './src/lib/templates/**/*.haml'
+  },
+  vendor:      {
+    scripts:     {
+      jquery:      './vendor/scripts/jquery/',
+      angular:     './vendor/scripts/angular/'
+    }
+  }
+};
+
+var destinations = {
+  docs:        {
+    images:      './dist/docs/images/',
+    scripts:     './dist/docs/scripts/',
+    stylesheets: './dist/docs/stylesheets/',
+    webfonts:    './dist/docs/webfonts/',
+    pages:       './dist/docs/'
+  },
+  lib:         {
+    scripts:     './dist/lib/scripts/',
+    stylesheets: './dist/lib/stylesheets/',
+    templates:   './disc/lib/templates/'
+  },
+  vendor:         {
+    scripts:     './dist/lib/scripts/'
+  }
+};
+
+
+
+gulp.task('docs-images', function() {
+  gulp.src(sources.docs.images)
+    .pipe(gulp.dest(destinations.docs.images))
+});
+
+gulp.task('docs-scripts', function() {
+  gulp.src(sources.docs.scripts)
+    .pipe(coffee())
+    .pipe(gulp.dest(destinations.docs.scripts))
+});
+
+gulp.task('docs-stylesheets', function() {
+  gulp.src(sources.docs.stylesheets)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest(destinations.docs.stylesheets))
+});
+
+gulp.task('docs-webfonts', function() {
+  gulp.src(sources.docs.webfonts)
+    .pipe(gulp.dest(destinations.docs.webfonts))
+});
+
+gulp.task('docs-pages', function() {
+  gulp.src(sources.docs.pages)
+    .pipe(haml())
+    .pipe(gulp.dest(destinations.docs.pages))
+});
+
+gulp.task('lib-scripts', function() {
+  gulp.src(sources.lib.scripts)
+    .pipe(coffee())
+    .pipe(gulp.dest(destinations.lib.scripts))
+});
+
+gulp.task('lib-stylesheets', function() {
+  gulp.src(sources.lib.stylesheets)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest(destinations.lib.stylesheets))
+});
+
+gulp.task('lib-templates', function() {
+  gulp.src(sources.lib.templates)
+    .pipe(haml())
+    .pipe(gulp.dest(destinations.lib.templates))
+});
+
+gulp.task('vendor-scripts', function() {
+  gulp.src([sources.vendor.scripts.jquery, sources.vendor.scripts.angular])
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(destinations.vendor.scripts));
+});
+
+
+
+gulp.task('build', [
+  'vendor-scripts',
+  'lib-scripts',
+  'lib-stylesheets'
+//  'lib-templates',
+//  'docs-scripts',
+//  'docs-stylesheets',
+//  'docs-webfonts',
+//  'docs-images',
+//  'docs-pages'
+]);
+
+gulp.task('watch', function() {
+//  gulp.watch(sources.vendor.scripts.jquery, ['vendor-scripts']);
+//  gulp.watch(sources.vendor.scripts.angular, ['vendor-scripts']);
+//  gulp.watch(sources.lib.scripts, ['lib-scripts']);
+//  gulp.watch(sources.lib.stylesheets, ['lib-stylesheets']);
+//  gulp.watch(sources.lib.templates, ['lib-templates']);
+//  gulp.watch(sources.docs.scripts, ['docs-scripts']);
+//  gulp.watch(sources.docs.stylesheets, ['docs-stylesheets']);
+//  gulp.watch(sources.docs.webfonts, ['docs-webfonts']);
+//  gulp.watch(sources.docs.images, ['docs-images']);
+//  gulp.watch(sources.docs.pages, ['docs-pages']);
+});
+
+
+
+gulp.task('default', ['build', 'watch']);
